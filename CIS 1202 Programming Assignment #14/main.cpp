bb05c0402039abd8dfeccac9bd0dcc7578c98430
MaryD88 <marymichelle1118@gmail.com>
2025-04-27 20:10:01 -0400

    Initial Commit

---
+++ CIS 1202 Programming Assignment #14/main.cpp
@@ -0,0 +1,14 @@
+// Mary Dalton
+// CIS 1202 501
+// April 27, 2025
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
